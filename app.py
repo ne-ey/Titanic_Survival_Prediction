69381c27da7472e6b1c3852e5942f5058726aa1a
Neha Pandey <np081616@gmail.com>
2025-11-27 01:04:43 +0530

    update

--- app.py
+++ app.py
@@ -7,9 +7,10 @@ app = Flask(__name__, template_folder="templates", static_folder="static")
 # ----------------------------
 # CONFIG - Replace these values
 # ----------------------------
-API_KEY = os.environ.get("API_KEY")
-DEPLOYMENT_URL = os.environ.get("DEPLOYMENT_URL")
 
+API_KEY = "BHQwK93PebW0Cs5Gf36GUqwMYnjxN6qyMxc0T1d46NYL"
+
+DEPLOYMENT_URL = "https://au-syd.ml.cloud.ibm.com/ml/v4/deployments/6b6c9b40-1066-4e05-9e6b-02589de9eafa/predictions?version=2021-05-01"
 
 def get_token():
     url = "https://iam.cloud.ibm.com/identity/token"
